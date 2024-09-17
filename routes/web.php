9a15ddb58a631225911fe0f71b45e499b0e90b7f
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-17 23:28:07 +0200

    create job

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,32 @@ Route::get('/', function () {
 
 
 Route::get("/jobs", function() {
-    return view('home',[
+    return view('jobs.index',[
         'jobs'=> Job::all()
     ]);
+});
+
+Route::get('/jobs/create', function(){
+    return view('jobs.create');    
 });
 
 Route::get("/jobs/{id}", function($id){
  
     $job = Job::find($id);
-    return view("job", ['job'=> $job]);
+    return view("jobs.show", ['job'=> $job]);
 });
+
+Route::post("/jobs", function(){
+ 
+    Job::create([
+        'title'=>'',
+        'salary'=>'',
+        'employer_id'=>'',
+    ]);
+    return redirect('/jobs');
+});
+
+
 
 Route::get("/contact", function(){
     return view("contact");
