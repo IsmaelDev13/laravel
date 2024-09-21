9580df9487c9e41634bb075a766b0b01e155d0ac
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-21 18:18:10 +0200

    authorization

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
@@ -35,9 +38,13 @@ class JobController extends Controller
           return redirect('/jobs');
     }
     public function edit(Job $job){
+      
+       
         return view("jobs.edit", ['job'=> $job]);
     }
     public function update(Request $request, Job $job){
+
+    Gate::authorize('edit-job', $job);
     request()->validate([
         'title'=> ['required', 'min:3'],
         'salary'=>   ['required'] 
@@ -51,6 +58,9 @@ class JobController extends Controller
     return redirect('/jobs/', $job->id);
     }
     public function destroy(Job $job){
+
+        Gate::authorize('edit-job', $job);
+
         $job->delete();
 
         return redirect("/jobs");
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -5,6 +5,7 @@
   <h2 class="font-bold text-lg">{{$job->title}}</h2>
   <p>This job pays {{$job->salary}}</p>
 
+  @can('edit',$job)
   <p class="mt-6">
     <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
   </p>
