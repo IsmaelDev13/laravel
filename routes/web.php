ceb8e06af36a72c00497c4ea52cf87a08f248600
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-21 12:12:36 +0200

    route controller

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,10 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs',[JobController::class,'index']);
-Route::get('/jobs/create',[JobController::class,'create']);
-Route::get("/jobs/{job}",[JobController::class,'show']);
-Route::post("/jobs", [JobController::class,'store']);
-Route::get("/jobs/{id}/edit",[JobController::class,'edit']);
-Route::patch("/jobs/{job}",[JobController::class,'update']);
-Route::delete("/juri: jobs/{job}", [JobController::class,'destroy']);
+Route::view('/','home');
 
 
-Route::get("/contact", function(){
-    return view("contact");
-});
+
+Route::resource('jobs', JobController::class);
+
+Route::view("/contact", "contact");
